dea60227c74e754146c61d6560bef996491f738a
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-04-07 12:09:00 +0100

    Fix rubocop offences

--- lib/transaction_log.rb
+++ lib/transaction_log.rb
@@ -16,9 +16,9 @@ class TransactionLog
     history = "Date      || Credit  || Debit  || Balance \n"
     @log.reverse.each do |transaction|
       history += "#{transaction.date.strftime('%d-%m-%Y')} || "
-      history += "#{transaction.credit ? '%.2f' % transaction.credit : ''} || "
-      history += "#{transaction.debit ? '%.2f' % transaction.debit : ''} || "
-      history += "#{'%.2f' % transaction.balance} \n"
+      history += "#{transaction.credit ? format('%.2f', transaction.credit) : ''} || "
+      history += "#{transaction.debit ? format('%.2f', transaction.debit) : ''} || "
+      history += "#{format('%.2f', transaction.balance)} \n"
     end
 
     history
